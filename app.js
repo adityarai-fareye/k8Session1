ea8ce8f56a61c605d083a4a7c4f669eb74f9eda0
Aditya Rai <adityarai@FE2256-Dev-IN-MacBook-Pro.local>
2024-05-13 14:37:21 +0530

    leak added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3333
 
-const session = "JWT"
+const session = "276d586c4432d29c6954a9d23c69843c5d75d127"
 
 app.get('/', (request, response) => {
     response.send('Default Route')
