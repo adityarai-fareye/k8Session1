aab65b230f39a265931d38eb52cb8ca862bcc261
Aditya Rai <adityarai@FE2256-Dev-IN-MacBook-Pro.local>
2024-05-14 15:00:34 +0530

    initial

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = 3333
 const value1='suraj'
-const value2='AIzaSyAfHNk2CTX44LJiSKEYy1jmYgSuMk8IABA'
 
 
 app.get('/', (request, response) => {
