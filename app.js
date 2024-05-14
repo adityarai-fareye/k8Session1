0867680efe8a759e3e17d62b64db4858b8729dab
Aditya Rai <adityarai@FE2256-Dev-IN-MacBook-Pro.local>
2024-05-14 14:43:50 +0530

    commit_2

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = 3333
 const value1='suraj'
+const value2='AIzaSyAfHNk2CTX44LJiSKEYy1jmYgSuMk8IABA'
+
 
 app.get('/', (request, response) => {
     response.send('Default Route')
