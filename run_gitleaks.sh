7aa0e4093596c757c32aab0bad5157db4f21c21e
Aditya Rai <adityarai@FE2256-Dev-IN-MacBook-Pro.local>
2024-05-21 22:23:50 +0530

    gitleaks sh

---
+++ run_gitleaks.sh
@@ -0,0 +1,13 @@
+gitleaks_scan_pull_request() {
+    local GITHUB_TOKEN="$1"
+    
+    # Fetch the pull request
+    git fetch origin pull/${{ github.event.pull_request.number }}/head:pull_request_branch
+    
+    # Checkout the pull request branch
+    git checkout pull_request_branch
+    
+    # Run gitleaks
+    GITHUB_TOKEN="$GITHUB_TOKEN" gitleaks -c /github/workspace
+}
+
