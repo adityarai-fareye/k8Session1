657f093684e145cb1eeea3cfff521ef4124556c7
Aditya Rai <adityarai@FE2256-Dev-IN-MacBook-Pro.local>
2024-05-21 22:31:29 +0530

    gitleaks sh call func

--- run_gitleaks.sh
+++ run_gitleaks.sh
@@ -1,14 +1,15 @@
-gitleaks_scan_pull_request() {
-    local GITHUB_TOKEN="$1"
-    
-    # Fetch the pull request
-    git fetch origin pull/${{ github.event.pull_request.number }}/head:pull_request_branch
-    
-    # Checkout the pull request branch
-    git checkout pull_request_branch
-    
-    # Run gitleaks
-    GITHUB_TOKEN="$GITHUB_TOKEN" gitleaks -c /github/workspace
-}
+#!/bin/bash
 
-gitleaks_scan_pull_request
+# Usage: ./run_gitleaks.sh GITHUB_TOKEN PULL_REQUEST_NUMBER
+
+GITHUB_TOKEN="$1"
+PULL_REQUEST_NUMBER="$2"
+
+# Fetch the pull request branch
+git fetch origin pull/${PULL_REQUEST_NUMBER}/head:pull_request_branch
+
+# Checkout the pull request branch
+git checkout pull_request_branch
+
+# Run gitleaks
+GITHUB_TOKEN="$GITHUB_TOKEN" gitleaks -c /github/workspace
