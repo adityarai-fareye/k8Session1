4092b21cff4c1f1564415798f9d53682699fc14d
Aditya Rai <adityarai@FE2256-Dev-IN-MacBook-Pro.local>
2024-08-29 10:55:29 +0530

    commit

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ app.get('/', (request, response) => {
 })
 
 app.listen(PORT, () => console.log('server running on: ',PORT))
+
+gitleaks detect --redact -v --exit-code=2 --report-format=sarif --report-path=results.sarif --log-level=debug --log-opts=--no-merges --first-parent e73a44d998131a96a95fd00b6be333ad5bb3dc09^..3d6334b5a261445b324da6cc29f5e35f81efb269
+gitleaks detect --redact -v --exit-code=2 --report-format=sarif --report-path=results.sarif --log-level=debug --log-opts=--no-merges --first-parent e73a44d998131a96a95fd00b6be333ad5bb3dc09^..20849092156b04953da6648b9f07bd7d53ab13d7
