84079e4217568f41d67144fd291e4531e652e1bd
Aditya Rai <adityarai@FE2256-Dev-IN-MacBook-Pro.local>
2024-08-29 10:23:31 +0530

    commit

--- run_gitleaks.sh
+++ run_gitleaks.sh
@@ -22,9 +22,9 @@ check_gitleaks_installed() {
 
 execute_gitleaks() {
         echo "Running Gitleaks..."
-    echo "gitleaks detect --redact -v --exit-code=2 --report-format=sarif --report-path=results.sarif --log-level=debug --log-opts=--no-merges --first-parent $first_commit_sha^..$last_commit_sha"
+    echo "gitleaks cmd: gitleaks detect --redact -v --exit-code=2 --report-format=sarif --report-path=results.sarif --log-level=debug --log-opts=--no-merges --first-parent $first_commit_sha^..$last_commit_sha"
     echo "Running Gitleaks...2"
-    gitleaks detect --redact -v --exit-code=2 --report-format=sarif --report-path=results.sarif --log-level=debug --log-opts=--no-merges $first_commit_sha^..$last_commit_sha
+    gitleaks detect --redact -v --exit-code=2 --report-format=sarif --report-path=results.sarif --log-level=debug --log-opts=--no-merges --first-parent $first_commit_sha^..$last_commit_sha
     # echo "Fetching pull request #$PULL_REQUEST_NUMBER..."
     # git fetch origin pull/${PULL_REQUEST_NUMBER}/head:pull_request_branch
 
